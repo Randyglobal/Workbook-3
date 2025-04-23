84e4fc26c37ab246a99eb9e9a671ecbf2ff131b5
Randyglobal <randyitoe2255@gmail.com>
2025-04-23 17:38:35 -0400

    Exercising Java

---
+++ Mod01/PayRollCalculator/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+public class Employee {
+    private String employeeId = "";
+    private String name = "";
+    private double hoursWorked = 0;
+    private double payRate = 0;
+    private double grossPay = 0;
+
+    public Employee(String employeeId, String name, double hoursWorked, double payRate, double grossPay) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+        this.grossPay = grossPay;
+    }
+
+    public String getEmployeeId() {
+        return this.employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return this.hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return this.payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+    public double getGrossPay(double grossPay){
+        double grossPayCheck = hoursWorked * payRate;
+        return grossPayCheck;
+    }
+
+    public void setGrossPay(double grossPay) {
+        this.grossPay = grossPay;
+    }
+}
---
+++ Mod01/PayRollCalculator/src/main/java/com/pluralsight/PayRollCalculator.java
@@ -0,0 +1,68 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class PayRollCalculator {
+    static Scanner scanner = new Scanner(System.in);
+    static public String employeeId;
+    static public String name;
+    static public double hoursWorked;
+    static public double payRate;
+    static public double grossPay;
+    static String[] inputArray;
+
+
+    public static void main(String[] args) {
+        fileReader();
+//        Employee employee = token();
+//        assignToken(employee);
+    }
+    public static void fileReader(){
+        try {
+            FileReader fileReader = new FileReader("DataFiles\\employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String headerLine = bufferedReader.readLine();
+
+            String input;
+
+            while ((input = bufferedReader.readLine()) != null){
+                inputArray = input.split("\\|");
+                System.out.println(Arrays.toString(inputArray));
+                System.out.println(inputArray[0]);
+
+                Employee employee = token();
+                assignToken(employee);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+    }
+    public static String display( String message){
+        System.out.println(message);
+        return message;
+    }
+    public static Employee token(){
+        employeeId = inputArray[0];
+        name = inputArray[1];
+        hoursWorked = Double.parseDouble(inputArray[2]);
+        payRate = Double.parseDouble(inputArray[3]);
+        Employee employee = new Employee(employeeId, name, hoursWorked, payRate, grossPay);
+        return employee;
+    }
+    public static void assignToken(Employee employee){
+       employeeId = employee.getEmployeeId();
+        name = employee.getName();
+        hoursWorked = employee.getHoursWorked();
+        payRate = employee.getPayRate();
+        System.out.printf("Employee Id: %s, Name: %s, Gross Pay: $%.2f%n", employeeId, name, payRate);
+    }
+
+
+}
